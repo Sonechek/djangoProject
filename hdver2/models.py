7e9558672dce85063bac882510c0fcf8141caa37
Alexander Shipulin <71063702+Sonechek@users.noreply.github.com>
2024-06-06 15:34:36 +0500

    финальная версия

--- hdver2/models.py
+++ hdver2/models.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import User
 
 class Ticket(models.Model):
     text = models.TextField()
-    employee = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    employee = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='employee_tickets')
+    specialist = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='specialist_tickets')
     date_opened = models.DateTimeField()
     date_closed = models.DateTimeField(blank=False, null=True)
     priority = models.IntegerField(default=3)
@@ -12,3 +13,4 @@ class Ticket(models.Model):
 
     class Meta:
         ordering = ["is_closed", "-date_opened", "-priority"]
+
--- hdver2/views.py
+++ hdver2/views.py
@@ -4,18 +4,21 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required, permission_required
 from django.shortcuts import render
 from .models import Ticket
+from django.contrib.auth.models import User
+
 
 
 def index(request):
     return render(request, 'hd/index.html')
 
 
-@login_required
+@permission_required('hdver2.add_ticket')
 def form(request):
     tickets = Ticket.objects.all()
     return render(request, 'hd/form.html', {"tickets": tickets})
 
 
+@permission_required('hdver2.add_ticket')
 def create(request):
     if request.method == "POST":
         ticket = Ticket()
@@ -63,6 +66,7 @@ def close_ticket(request, id):
         ticket = Ticket.objects.get(id=id)
         ticket.is_closed = True
         ticket.date_closed = datetime.datetime.now()
+        ticket.specialist_id =  request.user.id
         ticket.save()
         return HttpResponseRedirect("/tickets")
     except Ticket.DoesNotExist:
@@ -72,6 +76,7 @@ def close_ticket(request, id):
 @permission_required('admin')
 def reports(request):
     tickets = Ticket.objects.all()
+    users = User.objects.all()
     tickets_count = tickets.count()
     tickets_closed_count = 0
     for ticket in tickets:
@@ -84,6 +89,6 @@ def reports(request):
         for ticket in tickets:
             if ticket.is_closed:
                 tickets_closed_count += 1
-        return render(request, 'hd/reports.html', {'tickets': tickets, 'tickets_count': tickets_count, 'tickets_closed_count': tickets_closed_count})
+        return render(request, 'hd/reports.html', {'tickets': tickets, 'tickets_count': tickets_count, 'tickets_closed_count': tickets_closed_count, 'users': users})
     else:
         return render(request, 'hd/reports.html', {'tickets': tickets, 'tickets_count': tickets_count, 'tickets_closed_count': tickets_closed_count})
